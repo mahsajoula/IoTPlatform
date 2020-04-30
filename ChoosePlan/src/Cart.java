a6fad76f57c0ce3959c695a2e5a10ea487c6d5c9
tavakoli <tavakoli@ossl.ir>
2020-04-30 21:53:10 +0430

    Initial Fields and Getter Setter Methods Added

--- ChoosePlan/src/Cart.java
+++ ChoosePlan/src/Cart.java
@@ -1,2 +1,63 @@
-public class Cart extends Database implements DatabaseImp  {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Cart extends Database implements DatabaseImp {
+
+    private String cartID;
+    private String userID;
+    private List<Plan> plans = new ArrayList<>();
+    private List<AddOn> addOns = new ArrayList<>();
+    private float totalPrice;
+
+    public Cart(String userID) {
+        super();
+        this.userID = userID;
+        this.cartID = userID + new Date();
+    }
+
+    public List<Plan> getPlans() {
+        return plans;
+    }
+
+    public void addPlan(Plan plans) {
+        this.plans.add(plans);
+    }
+
+    public List<AddOn> getAddOns() {
+        return addOns;
+    }
+
+    public void addAddOn(AddOn addOns) {
+        this.addOns.add(addOns);
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public boolean deletePlan(String planID) {
+        for (Plan plan : plans) {
+            if (plan.planID == planID) {
+                this.plans.remove(plan);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteAddOn(String addOnID) {
+        for (Plan addOn : addOns) {
+            if (addOn.addOnID == addOnID) {
+                this.plans.remove(addOn);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
