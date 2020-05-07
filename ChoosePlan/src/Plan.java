7e0c6003c663e5ab67dab04a57c217f39d2a1506
pouyeh.banijamli <banijamli@ossl.ir>
2020-05-07 13:26:49 +0430

    plan.v1

--- ChoosePlan/src/Plan.java
+++ ChoosePlan/src/Plan.java
@@ -1,2 +1,43 @@
 public class Plan extends Database implements DatabaseImp {
+     String id;
+     String name;
+     float price ;
+     String duration;
+     String PicAddress;
+     String userId;
+    
+     public Plan(String userId,String planId) {
+         super();
+         this.userId = userId;
+         this.id = planId; 
+     }
+    //  public String getId(){
+    //      return id;
+    //  }
+    //  public String getName(){
+    //      return name;
+    //  }
+    //  public float getPrice(){
+    //      return price;
+    //  }
+    //  public String getDuration(){
+    //      return duration;
+    //  }
+    //  public String getUserId(){
+    //      return userId;
+    //  }
+     public void setData(String name ,float Price , String duration, String PicAddress ){
+         this.insert("insert into Plans values ("+ name +','+ price + "," + duration +","+ PicAddress +")")
+     }
+     public void updateData(Object ){
+         this.update("update Plans where id="+this.id+"set")
+     }
+     public void deleteData(){
+         this.delete("delete from Plans where id="+this.id)
+     }
+     public void getData(){
+         this.select("select * from Plans where id="+this.id)
+
+     }
+
 }
