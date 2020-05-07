abd7370ab20b133ad1b79f441ba236b9ec7a9b0d
Hediyeh Aghajani <hedie.a91@gmail.com>
2020-05-07 21:50:55 +0430

    completed SubscriptionController class

--- ChoosePlan/src/SubscriptionController.java
+++ ChoosePlan/src/SubscriptionController.java
@@ -1,2 +1,36 @@
+
+
 public class SubscriptionController {
+
+    private AddOn addOn;
+    private Cart cart;
+    private Plan plan;
+
+
+    public boolean getSubscriptionsAndPlans() {
+        try {
+            for(int i = 0 ; i < 2 ; i ++) {
+                plan = new Plan(String.valueOf(i));
+                plan.getData();
+
+            }
+            for (int i = 0 ; i < 2 ; i ++) {
+                addOn = new AddOn(String.valueOf(i));
+                addOn.getData;
+            }
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+    
+    public boolean choosePlan(Cart data) {
+        try {
+            cart.setData(data);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
