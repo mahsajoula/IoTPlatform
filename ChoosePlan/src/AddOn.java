db0ea95514bab65826f9501a9a19e8de39f8fb99
zeynab.sobhani <sobhani@ossl.ir>
2020-05-07 22:31:08 +0430

    AddOn class added

--- ChoosePlan/src/AddOn.java
+++ ChoosePlan/src/AddOn.java
@@ -1,2 +1,36 @@
 public class AddOn extends Database implements DatabaseImp {
+    private String addOnID;
+    private String addOnName;
+    private string addOnPic;
+    private string addOnPrice;
+    private List<Service> services = new ArrayList<>();
+
+    public AddOn() {
+        super();
+        this.addOnID = addOnID
+    }
+
+    @override
+    public void setData() {
+
+    }
+
+    @override
+    public void updateData() {
+    }
+
+    @override
+    public void deleteData() {
+
+    }
+
+    @override
+    public void getData(String addOnID) {
+        AddOn addOn = (AddOn)  this.read("SELECT * FROM AddOns WHERE id =" + this.addOnID);
+        this.addOnName = addOn.addOnName;
+        this.addOnPic = addOn.addOnPic;
+        this.addOnPrice = addOn.addOnPrice;
+        this.services = addOn.services;
+    }
+
 }
