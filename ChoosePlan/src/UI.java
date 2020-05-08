d16b5d0e1f5bb6596a035c0a1e5c660ed32a39f0
mahsajoula <64605833+mahsajoula@users.noreply.github.com>
2020-05-08 14:30:35 +0430

    Update UI.java

--- ChoosePlan/src/UI.java
+++ ChoosePlan/src/UI.java
@@ -1,3 +1,25 @@
 public class UI {
-
+SubscriptionController ob=new SubscriptionController(); // creating an Object
+	static void ChoosePlan(){
+		try{
+			boolean SubscriptionsAndPlansLists=ob.getSubscriptionsAndPlans(); //calling first method
+			System.out.println(SubscriptionsAndPlansLists);
+			try{
+				boolean choosePlans=ob.choosePlan();  //calling second method
+				System.out.println(choosePlans);
+			}
+			catch (Exception e) {
+				System.out.println("Something went wrong.");
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Something went wrong.");
+		}
+	}
+	public static void main (String[ ] args) {
+		
+		ChoosePlan();
+ 	
+	
+	}
 }
